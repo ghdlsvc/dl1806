f609bd8eaa49c85549b107d43a16ba1e886f87b6
Helmi Amirudin <helmilaw@gmail.com>
2023-06-19 10:37:10 +0700

    Update runme.sh

--- scripts/runme.sh
+++ scripts/runme.sh
@@ -5,7 +5,7 @@ echo -e "  of HelmiWrt OS Releases"
 echo -e "===================="
 
 anu="$GITHUB_WORKSPACE/helmiwrt18"
-curl -s "https://api.github.com/repos/ghdlsvc/dl21/releases" | grep helmiwrt-18.06-k5.4-"${1}" |  sed 's|[[: \":]]||g' | awk -F 'browser_download_url' '{print$2}' | sed '/^[[:space:]]*$/d' > ${anu}
+curl -sL "https://api.github.com/repos/ghdlsvc/dl21/releases" | grep helmiwrt-18.06-k5.4-"${1}" |  sed -e 's|\"||g'  -e  's/://g' | awk -F 'browser_download_url' '{print$2}' | sed -e '/^[[:space:]]*$/d' > ${anu}
 mkdir $GITHUB_WORKSPACE/maknyus
 cd $GITHUB_WORKSPACE/maknyus
 wget -i ${anu}
